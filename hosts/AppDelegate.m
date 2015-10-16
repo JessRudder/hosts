e79a64d98bce5b6486e27d4ccaeaeaf8e29ef0e3
JessRudder <thejessicarudder@gmail.com>
2015-10-15 22:13:43 -0400

    Wire up button to fire off script

--- hosts/AppDelegate.m
+++ hosts/AppDelegate.m
@@ -24,5 +24,21 @@
 }
 
 - (IBAction)updateHostFile:(id)sender {
+    // path to the installHosts shell script within project
+    NSString *shellScriptPath = [[NSBundle mainBundle] pathForResource:@"installHosts" ofType:@"sh"];
+
+    // prepend bash path to run installHosts shell script
+    NSString *shellScriptRunCommand = [NSString stringWithFormat: @"\"/bin/bash %@\"", shellScriptPath];
+    
+    // build AppleScript command for running shell script with permissions
+    NSString *appleScriptCommand = [[NSString alloc] initWithFormat:@"do shell script %@ with administrator privileges", shellScriptRunCommand];
+    
+    // type to AppleScript
+    NSAppleScript *shellScript;
+    shellScript = [[NSAppleScript alloc] initWithSource:appleScriptCommand];
+    
+    // run script
+    NSDictionary *errorsDictionary = NULL;
+    [shellScript executeAndReturnError:&errorsDictionary];
 }
 @end
